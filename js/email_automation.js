90a6d6affe43076812cb1d238ca69b26ac461d3e
Alex <124251221+PWRXNDR@users.noreply.github.com>
2024-03-18 07:32:01 +0100

    Add files via upload

--- js/email_automation.js
+++ js/email_automation.js
@@ -19,3 +19,54 @@ document.addEventListener('DOMContentLoaded', function () {
         circle.addEventListener('click', handleCircleClick);
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const form = document.querySelector('.contact-form');
+    const formAction = form.getAttribute('action');
+    const formMessage = document.getElementById('form-message');
+
+    function displayMessage(msg, isError = false) {
+        formMessage.textContent = msg;
+        formMessage.style.display = 'block';
+        formMessage.className = isError ? 'form-message error' : 'form-message';
+    }
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        const formData = new FormData(form);
+        const submitButton = form.querySelector('.submit-btn');
+        submitButton.textContent = 'Sending...';
+        submitButton.disabled = true;
+
+        fetch(formAction, {
+            method: 'POST',
+            body: formData,
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+            .then(response => {
+                if (response.ok) {
+                    displayMessage('Message Sent!');
+                    form.reset(); // To reset the form fields after submission
+                } else {
+                    response.json().then(data => {
+                        if (Object.hasOwn(data, 'errors')) {
+                            displayMessage('Errors in form', true);
+                        } else {
+                            displayMessage('An error occurred', true);
+                        }
+                    })
+                }
+            })
+            .catch(error => {
+                displayMessage('An error occurred', true);
+            })
+            .finally(() => {
+                submitButton.disabled = false;
+                submitButton.textContent = 'Send Message';
+            });
+    });
+});
